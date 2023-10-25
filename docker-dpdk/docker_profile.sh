aa82d955b835c05bd9ea99d6585f514f9a790f18
marezayeye <marezayeye@gmail.com>
2023-10-25 15:16:23 +0330

    Updated Dockerfile and scripts

--- docker-dpdk/docker_profile.sh
+++
@@ -1,4 +0,0 @@
-export RTE_SDK=/root/dpdk-23.07
-export PKTGEN=/root/pktgen-dpdk
-export RTE_TARGET=build
-alias menu='source $RTE_SDK/tools/setup.sh'
---
+++ pktgen-dpdk-docker/scripts/dpdk-profile.sh
@@ -0,0 +1,3 @@
+export RTE_SDK=/root/dpdk-23.07
+export PKTGEN=/root/pktgen-dpdk
+export RTE_TARGET=build
